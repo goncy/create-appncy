cc1bf6dba3292078ad11c3017f631200e7426bb4
Bruno Hartmann <brhartmann90@gmail.com>
2023-11-03 22:41:49 -0300

    Updated eslint

--- dist/templates/react-eslint-ts-tw/project/src/App.tsx
+++ dist/templates/react-eslint-ts-tw/project/src/App.tsx
@@ -1,10 +1,11 @@
 function App() {
+  const appName = '{{name}}';
   return (
     <main className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
-      <header className="text-xl font-bold leading-[4rem]">{{name}}</header>
+      <header className="text-xl font-bold leading-[4rem]">{appName}/header>
       <section className="py-8">👋</section>
       <footer className="text-center leading-[4rem] opacity-70">
-        © {new Date().getFullYear()} {{name}}
+        © {new Date().getFullYear()} {appName}
       </footer>
     </main>
   );
---
+++ index.mjs
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { cp, readFile, writeFile } from 'node:fs/promises';
+import { glob } from 'glob';
+import color from 'picocolors';
+import prompts from 'prompts';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+// Specify CLI arguments
+const args = yargs(hideBin(process.argv)).options({
+  name: {
+    alias: 'n',
+    type: 'string',
+    description: 'Name of the project'
+  },
+  template: {
+    alias: 't',
+    type: 'string',
+    description: 'Template to use'
+  }
+});
+
+// Orverride arguments passed on the CLI
+prompts.override(args.argv);
+
+async function main() {
+  const project = await prompts(
+    [
+      {
+        type: 'text',
+        name: 'name',
+        message: 'What is the name of your project?',
+        initial: 'appncy-project',
+        validate: (value) => {
+          if (value.match(/[^a-zA-Z0-9-_]+/g))
+            return 'Project name can only contain letters, numbers, dashes and underscores';
+
+          return true;
+        }
+      },
+      {
+        type: 'select',
+        name: 'template',
+        message: `Which template would you like to use?`,
+        initial: 0,
+        choices: [
+          { title: 'Next.js + ESLint + TypeScript + Tailwind', value: 'next-eslint-ts-tw' },
+          { title: 'React (vite) + ESLint + TypeScript + Tailwind', value: 'react-eslint-ts-tw' }
+        ]
+      }
+    ],
+    {
+      onCancel: () => {
+        console.log('\nBye 👋\n');
+
+        process.exit(0);
+      }
+    }
+  );
+
+  const template = path.join(path.dirname(fileURLToPath(import.meta.url)), 'templates', project.template);
+  const destination = path.join(process.cwd(), project.name);
+
+  // Copy files from the template folder to the current directory
+  await cp(template, destination, { recursive: true });
+
+  // Get all files from the destination folder
+  const files = await glob(`**/*`, { nodir: true, cwd: destination, absolute: true });
+
+  // Read each file and replace the tokens
+  for await (const file of files) {
+    const data = await readFile(file, 'utf8');
+    const draft = data.replace(/{{name}}/g, project.name);
+
+    await writeFile(file, draft, 'utf8');
+  }
+
+  // Log outro message
+  console.log('✨ Project created ✨');
+  console.log(`\n${color.yellow(`Next steps:`)}\n`);
+  console.log(`${color.green(`cd`)} ${project.name}`);
+  console.log(`${color.green(`pnpm`)} install`);
+  console.log(`${color.green(`pnpm`)} dev`);
+  console.log('\n---\n');
+  console.log(`Questions 👀? ${color.underline(color.cyan('https://x.com/goncy'))}`);
+}
+
+main().catch(console.error);

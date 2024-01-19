ba119a48267c16864b2e344639cedd7d6e3ae27c
goncy <gonzalo.pozzo4@gmail.com>
2024-01-19 09:00:36 -0300

    Add link

--- templates/next-eslint-ts-shadcn/project/src/app/layout.tsx
+++ templates/next-eslint-ts-shadcn/project/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type {Metadata} from "next";
 
+import {Link} from "next/link"
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -11,7 +13,9 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className="dark container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] bg-background px-4 font-sans antialiased">
-        <header className="text-xl font-bold leading-[4rem]">{{name}}</header>
+        <header className="text-xl font-bold leading-[4rem]">
+          <Link href="/">{{name}}</Link>
+        </header>
         <main className="py-8">{children}</main>
         <footer className="text-center leading-[4rem] opacity-70">
           © {new Date().getFullYear()} {{name}}
--- templates/next-eslint-ts-tw/project/src/app/layout.tsx
+++ templates/next-eslint-ts-tw/project/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type {Metadata} from "next";
 
+import {Link} from "next/link"
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -10,8 +12,10 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
-        <header className="text-xl font-bold leading-[4rem]">{{name}}</header>
+      <body className="dark container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] bg-background px-4 font-sans antialiased">
+        <header className="text-xl font-bold leading-[4rem]">
+          <Link href="/">{{name}}</Link>
+        </header>
         <main className="py-8">{children}</main>
         <footer className="text-center leading-[4rem] opacity-70">
           © {new Date().getFullYear()} {{name}}

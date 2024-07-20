8188e42084fd77ef95cbf2b5f32124f9673170cd
goncy <gonzalo.pozzo4@gmail.com>
2024-07-20 14:50:20 -0300

    Remove dist from git

--- dist/templates/next-eslint-ts-shadcn/extras/libsql/src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import Database from "libsql";
-import {drizzle} from "drizzle-orm/better-sqlite3";
-
-import * as schema from "./schema";
-
-const raw = new Database("data.db");
-const db = drizzle(raw, {schema});
-
-export default db;
--- dist/templates/next-eslint-ts-shadcn/project/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import {Button} from "@/components/ui/button";
-
-export default function HomePage() {
-  return <Button>👋</Button>;
-}

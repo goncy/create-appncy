dcdcde03fa7ae59e430fa9cf543a95c46676218e
goncy <gonzalo.pozzo4@gmail.com>
2026-04-23 16:40:27 -0300

    bump version

--- templates/react-eslint-ts-tw/project/src/App.tsx
+++ templates/react-eslint-ts-tw/project/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <main className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
-      <header className="text-xl leading-[4rem] font-bold">{{name}}</header>
+      <header className="text-xl leading-16 font-bold">{{name}}</header>
       <section className="py-8">👋</section>
-      <footer className="text-center leading-[4rem] opacity-70">
+      <footer className="text-center leading-16 opacity-70">
         © {new Date().getFullYear()} {{name}}
       </footer>
     </main>
